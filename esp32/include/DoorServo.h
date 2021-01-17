690f6ba5e8575bf78d8beb6afc8fcead83ada431
Vilhelm Prytz <vilhelm@prytznet.se>
2021-01-17 23:17:32 +0100

    esp32: change door servo turn time

--- esp32/include/DoorServo.h
+++ esp32/include/DoorServo.h
@@ -34,7 +34,7 @@ public:
     {
         setup();
         servo.write(pos);
-        delay(3000);    // this is how long the servo will turn for
+        delay(1000);    // this is how long the servo will turn for
         servo.detach();
     }
 
